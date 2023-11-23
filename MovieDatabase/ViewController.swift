1a0fc042fd52e357dc6886e691f9564a96a7012e
simkyt <99974435+simkyt@users.noreply.github.com>
2023-11-23 19:01:36 +0200

    Initial Commit

---
+++ MovieDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDatabase
+//
+//  Created by Simonas Kytra on 23/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
